fe304d551d51870e9daf2f9877b5d829e0ce262b
Octocat <npolevara@gmail.com>
2016-12-13 22:09:37 +0200

    separate reducer

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,24 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class App extends Component {
+class App extends Component {
   render() {
-    return <div>Привет, ты вкурсе что ты невнимательная скотина</div>
-
+    const { name, surname, age } = this.props.user
+    const { year, photos } = this.props.page
+    return (
+      <div>
+        <div>Привет, { name } ты вкурсе что ты же { surname } и тебе { age }</div>
+        <div>У тебя { photos.length } за { year } год</div>
+      </div>
+    )
   }
 }
+
+function mapStateToProps (state) {
+  return {
+    user: state.user,
+    page: state.page
+  }
+}
+
+export default connect(mapStateToProps)(App)
---
+++ src/reducers/user.js
@@ -0,0 +1,9 @@
+const initialState = {
+  name: 'Никита',
+  surname: 'Полевара',
+  age: 33
+};
+
+export default function userstate(state = initialState) {
+  return state;
+}
