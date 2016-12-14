960f7c34afee489a0ca6a211cb127723f46afc8c
Octocat <npolevara@gmail.com>
2016-12-14 21:31:10 +0200

    add css loaders and styles

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './containers/App'
 import configureStore from './store/configureStore'
+import './styles/app.css'
 
 const store = configureStore()
 
 render(
   <Provider store={store}>
-    <App />
-  </Provider>
-  , document.getElementById('root')
+    <div className='app'>
+      <App />
+    </div>
+  </Provider>,
+  document.getElementById('root')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 var path = require('path');
 var webpack = require('webpack');
 var NpmInstallPlugin = require('npm-install-webpack-plugin');
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
+
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
@@ -39,7 +42,14 @@ module.exports = {
         ],
         test: /\.js$/,
         plugin: ['transform-runtime'],
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader!postcss-loader"
       }
     ]
+  },
+  postcss: function() {
+    return [autoprefixer, precss];
   }
 }
